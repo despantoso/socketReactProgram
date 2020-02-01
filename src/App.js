81121c340bba7e80c0950f2f3473c84839ca9cf1
despantoso@ufl.edu <despantoso@ufl.edu>
2020-02-01 14:22:09 -0500

    Connection works, however button feature does not. Can only refresh for it to work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import io from "socket.io-client";
+import socket from "socket.io-client";
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <button onclick={ io("http://10.136.104.49:48902").emit("registerShelter", "")}>BUTTON</button>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +24,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <br />
         </a>
       </header>
     </div>
